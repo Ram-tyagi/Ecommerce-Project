132476f5d9bc62dabf10832eff0d4d3a784687cd
Ram-tyagi <ramtyagi04@gmail.com>
2023-08-07 18:12:44 +0530

    About.us

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 
-
+import {CreateBrowserRouter,RouterProvider} from 'react-router-dom';
 import CartProvider from './Store/CartProvider';
 import Header from './Components/Header'
 import Footer from './Components/Footer';
 import Products from './Components/Product';
+import About from './Components/About';
 
-
+const router=CreateBrowserRouter([
+  {path: '/', Element: <About/>},
+])
 function App() {
   return (
     <CartProvider>
-      
+      <RouterProvider router={router}/>
       <Header />
       <Products />
       <Footer />
---
+++ src/Components/About.js
@@ -0,0 +1,50 @@
+import React from "react";
+import classes from "./About.module.css";
+const About = () => {
+  return (
+    <section className={classes.section}>
+      <h1>The Generics</h1>
+      <h2>About</h2>
+      <div>
+        <img
+          src="https://prasadyash2411.github.io/ecom-website/img/Band%20Members.png"
+          alt="Musical Band"
+        />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Donec
+          pretium vulputate sapien nec sagittis. Vulputate dignissim suspendisse
+          in est ante in nibh. Enim diam vulputate ut pharetra sit amet.
+          Tincidunt dui ut ornare lectus sit amet est placerat in. Placerat orci
+          nulla pellentesque dignissim. Semper auctor neque vitae tempus. Ac
+          auctor augue mauris augue neque. Purus in massa tempor nec feugiat
+          nisl pretium. Porta nibh venenatis cras sed felis eget velit. Lorem
+          donec massa sapien faucibus. Sit amet cursus sit amet dictum. Eu non
+          diam phasellus vestibulum lorem sed risus ultricies tristique. Elit
+          sed vulputate mi sit. Non curabitur gravida arcu ac tortor dignissim.
+          Neque ornare aenean euismod elementum nisi quis eleifend. Ac tortor
+          dignissim convallis aenean. Id cursus metus aliquam eleifend mi in
+          nulla. Sed pulvinar proin gravida hendrerit. Amet mauris commodo quis
+          imperdiet massa tincidunt nunc pulvinar. Leo urna molestie at
+          elementum eu facilisis sed odio morbi. Cursus sit amet dictum sit amet
+          justo donec enim. Molestie nunc non blandit massa enim nec dui nunc.
+          Elementum eu facilisis sed odio morbi quis commodo odio. Viverra
+          tellus in hac habitasse. Fermentum posuere urna nec tincidunt praesent
+          semper feugiat. Est placerat in egestas erat imperdiet.Semper auctor
+          neque vitae tempus. Ac auctor augue mauris augue neque. Purus in massa
+          tempor nec feugiat nisl pretium. Porta nibh venenatis cras sed felis
+          eget velit. Lorem donec massa sapien faucibus. Sit amet cursus sit
+          amet dictum. Eu non diam phasellus vestibulum lorem sed risus
+          ultricies tristique. Elit sed vulputate mi sit. Non curabitur gravida
+          arcu ac tortor dignissim. Neque ornare aenean euismod elementum nisi
+          quis eleifend. Ac tortor dignissim convallis aenean. Id cursus metus
+          aliquam eleifend mi in nulla. Sed pulvinar proin gravida hendrerit.
+          Amet mauris commodo quis imperdiet massa tincidunt nunc pulvinar. Id
+          cursus metus aliquam eleifend mi in nulla.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
